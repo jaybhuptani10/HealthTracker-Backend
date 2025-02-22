3d740ac6583870ff36b4dc672c25382865c5a565
unknown <jaybhuptani1054@gmail.com>
2025-02-22 19:55:03 +0530

    try-1

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const http = require('http');
 const app = require('./app');
-const port = process.env.PORT || 3000;
-const server = http.createServer(app);
+
 const connectToDb = require('./db/db');
 const dotenv = require('dotenv');
 dotenv.config({ path: './.env' });
