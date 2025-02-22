b75fdbf9204c88ab9c22b5139fc483b36d208a57
unknown <jaybhuptani1054@gmail.com>
2025-02-22 19:42:28 +0530

    check3

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.get('/', (req, res) => {
-  res.send('Hello World kese ho');
+app.use('/', (req, res) => {
+  res.json("Hell");
 });
 app.use('/user', userRoutes);
 module.exports = app;
