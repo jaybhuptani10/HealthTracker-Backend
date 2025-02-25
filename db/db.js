0909e7ce19eb2d72011bc6e272d4d3bd527f3e3c
unknown <jaybhuptani1054@gmail.com>
2025-02-25 10:24:39 +0530

    ENV

--- db/db.js
+++ db/db.js
@@ -4,4 +4,4 @@ function connect() {
   return mongoose.connect(process.env.DB_CONNECTION_STRING,).then(console.log('Connected to database'));
 }
 
-module.exports =  connect ;
+module.exports =  connect;
